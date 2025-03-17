b381bb896aa2ba75a97deb8891d9d8d8fba437ed
Chandra <152131475+ChandraPahlawan@users.noreply.github.com>
2025-03-17 20:21:37 +0800

    Add files via upload

---
+++ posttest1_pemvis/posttest1_pemvis/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        tbips = New TextBox()
+        tbipk = New TextBox()
+        btntambah = New Button()
+        btnreset = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        LabelPredikat = New Label()
+        SuspendLayout()
+        ' 
+        ' tbips
+        ' 
+        tbips.Location = New Point(238, 110)
+        tbips.Name = "tbips"
+        tbips.Size = New Size(186, 27)
+        tbips.TabIndex = 0
+        ' 
+        ' tbipk
+        ' 
+        tbipk.Location = New Point(237, 204)
+        tbipk.Name = "tbipk"
+        tbipk.Size = New Size(189, 27)
+        tbipk.TabIndex = 1
+        ' 
+        ' btntambah
+        ' 
+        btntambah.Location = New Point(238, 163)
+        btntambah.Name = "btntambah"
+        btntambah.Size = New Size(82, 35)
+        btntambah.TabIndex = 2
+        btntambah.Text = "Tambah"
+        btntambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnreset
+        ' 
+        btnreset.Location = New Point(349, 168)
+        btnreset.Name = "btnreset"
+        btnreset.Size = New Size(77, 30)
+        btnreset.TabIndex = 3
+        btnreset.Text = "Reset"
+        btnreset.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(135, 117)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 20)
+        Label1.TabIndex = 4
+        Label1.Text = "IP Semester"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(132, 204)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 20)
+        Label2.TabIndex = 5
+        Label2.Text = "IP Kumulatif"
+        ' 
+        ' LabelPredikat
+        ' 
+        LabelPredikat.AutoSize = True
+        LabelPredikat.Location = New Point(251, 250)
+        LabelPredikat.Name = "LabelPredikat"
+        LabelPredikat.Size = New Size(0, 20)
+        LabelPredikat.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(LabelPredikat)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btnreset)
+        Controls.Add(btntambah)
+        Controls.Add(tbipk)
+        Controls.Add(tbips)
+        Name = "Form1"
+        Text = "2309106011"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents tbips As TextBox
+    Friend WithEvents tbipk As TextBox
+    Friend WithEvents btntambah As Button
+    Friend WithEvents btnreset As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LabelPredikat As Label
+
+End Class
---
+++ posttest1_pemvis/posttest1_pemvis/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btntambah.Click
+        Dim ipSemester As Double
+
+        ' Validasi input angka dan batas maksimum IP Semester = 5
+        If Double.TryParse(tbips.Text, ipSemester) Then
+            If ipSemester > 5 Then
+                MessageBox.Show("IP Semester tidak boleh lebih dari 5!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                tbips.Clear()
+                tbips.Focus()
+                Return
+            End If
+
+            totalIP += ipSemester
+            jumlahSemester += 1
+
+            ' Hitung IP Kumulatif
+            Dim ipk As Double = totalIP / jumlahSemester
+
+            ' Batasi IPK maksimal 4.0
+            If ipk > 4 Then
+                ipk = 4
+            End If
+
+            tbipk.Text = ipk.ToString("0.00")
+
+            ' Tentukan Predikat
+            If ipk >= 3.01 Then
+                LabelPredikat.Text = "Sangat Memuaskan"
+            ElseIf ipk >= 2.76 Then
+                LabelPredikat.Text = "Memuaskan"
+            ElseIf ipk >= 2.0 Then
+                LabelPredikat.Text = "Cukup"
+            Else
+                LabelPredikat.Text = "Kurang"
+            End If
+
+            ' Kosongkan input
+            tbips.Clear()
+            tbips.Focus()
+        Else
+            MessageBox.Show("Masukkan angka yang valid!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnreset.Click
+        totalIP = 0
+        jumlahSemester = 0
+        tbipk.Clear()
+        LabelPredikat.Text = ""
+        tbips.Clear()
+        tbips.Focus()
+    End Sub
+End Class
